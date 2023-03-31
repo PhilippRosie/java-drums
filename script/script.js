36f0ace21478a761021bb88e9cb784746f3070b5
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-03-31 13:36:37 +0200

    first commit

--- script/script.js
+++ script/script.js
@@ -18,3 +18,17 @@ function playSound(e) {
 }
 
 window.addEventListener("keydown", playSound);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
